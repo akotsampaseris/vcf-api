d1e89a9bcdd52347161a60bca5fe7a8950ee96cc
akotsampaseris <a.kotsampaseris@gmail.com>
2022-09-30 00:07:59 +0100

    Testing suite updated

--- tests/test_requests/test_vcf_data_row.py
+++ tests/test_requests/test_vcf_data_row.py
@@ -1,46 +1,153 @@
 import os
-from dotenv import load_dotenv
-import pytest
+import json
 import requests
-from django.urls import reverse
 from rest_framework import status
+from dotenv import load_dotenv
 
 load_dotenv()
 
 # Create your tests here.
-class TestVcfDataList:
-    def test_get_data_list(self):
+class TestVcfDataRow:
+    def test_create_new_data_row(self):
         domain = os.environ.get('APP_DOMAIN')
-        url = '/'.join([domain, 'api/data/list'])
+        url = '/'.join([domain, 'api/data/row'])
         
-        headers = {'Accept': '*/*'}
-        response = requests.get(url, headers=headers)
-        results = response.json()
-        
-        assert response.status_code == status.HTTP_200_OK
-        assert results['count'] > 0
-        assert 'application/json' in response.headers[
-            'Content-Type'
-        ]
+        payload = {
+            "ID": "rs1",
+            "CHROM": "chr10",
+            "POS": 1000,
+            "ALT": "A",
+            "REF": "G"
+        }
 
-
-        headers = {'Accept': 'application/xml'}
-        response = requests.get(url, headers=headers)
-
-        assert 'application/xml' in response.headers[
-            'Content-Type'
-        ]
-        
-
-        headers = {'Accept': 'wrong/header'}
-        response = requests.get(url, headers=headers)
+        response = requests.post(url, data=payload)
         
         assert response.status_code \
-            == status.HTTP_406_NOT_ACCEPTABLE
+            == status.HTTP_403_FORBIDDEN
+        
+        
+        headers = {
+            'Authorization': 'somesecretkey'
+        }
+
+        response = requests.post(
+            url, data=json.dumps(payload), headers=headers
+        )
+        results = response.json()
+        
+        assert response.status_code \
+            == status.HTTP_201_CREATED
+        assert payload == results
 
 
-        params = {'id': 'b@dvalu3#'}
-        response = requests.get(url, params=params)
+        payload = {
+            "CHROM": "chr10",
+            "POS": 1000,
+            "ALT": "A",
+            "REF": "G"
+        }
+
+        response = requests.post(
+            url, data=json.dumps(payload), headers=headers
+        )
+        results = response.json()
+
+        assert response.status_code \
+            == status.HTTP_400_BAD_REQUEST
+        assert results['ID']
+        assert len(results['ID']) == 1
+
+    
+    def test_update_existing_data_row(self):
+        domain = os.environ.get('APP_DOMAIN')
+        url = '/'.join([domain, 'api/data/row'])
+        
+        payload = {
+            "ID": "rs1",
+            "CHROM": "chr10",
+            "POS": 1000,
+            "ALT": "A",
+            "REF": "G"
+        }
+
+        response = requests.put(url, data=payload)
+        
+        assert response.status_code \
+            == status.HTTP_403_FORBIDDEN
+        
+        
+        headers = {
+            'Authorization': 'somesecretkey'
+        }
+
+        response = requests.put(
+            url, data=json.dumps(payload), headers=headers
+        )
+        results = response.json()
         
         assert response.status_code \
             == status.HTTP_400_BAD_REQUEST
+
+        response = requests.put(
+            url, 
+            params={'id': 'rs1'},
+            data=json.dumps(payload), 
+            headers=headers
+        )
+        results = response.json()
+        
+        assert response.status_code \
+            == status.HTTP_200_OK
+        assert payload == results
+
+
+        payload = {
+            "CHROM": "chr10",
+            "POS": 1000,
+            "ALT": "A",
+            "REF": "G"
+        }
+
+        response = requests.put(
+            url, 
+            params={'id': 'rs1'},
+            data=json.dumps(payload), 
+            headers=headers
+        )
+        results = response.json()
+
+        assert response.status_code \
+            == status.HTTP_400_BAD_REQUEST
+        assert results['ID']
+        assert len(results['ID']) == 1
+
+
+    def test_delete_existing_data_row(self):
+        domain = os.environ.get('APP_DOMAIN')
+        url = '/'.join([domain, 'api/data/row'])
+
+        response = requests.delete(url)
+        
+        assert response.status_code \
+            == status.HTTP_403_FORBIDDEN
+        
+        
+        headers = {
+            'Authorization': 'somesecretkey'
+        }
+
+        response = requests.put(
+            url, headers=headers
+        )
+        
+        assert response.status_code \
+            == status.HTTP_400_BAD_REQUEST
+
+        response = requests.delete(
+            url, 
+            params={'id': 'rs1'}, 
+            headers=headers
+        )
+        
+        assert response.status_code \
+            == status.HTTP_204_NO_CONTENT
